655a7619abd714106840ac8b4e3028da4613f6c1
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-07-30 13:32:21 +0200

    timed version of the solver

--- src/solver.jl
+++ src/solver.jl
@@ -1,3 +1,4 @@
+using TimerOutputs
 """
     x = solve(c::AbstractCompositeModel, x::SVector, vars, others; tol = 1.0e-9, itermax = 1e4, verbose=true)
 
@@ -25,6 +26,33 @@ function solve(c::AbstractCompositeModel, x::SVector, vars, others; tol::Float64
     end
     return x
 end
+
+
+function solve_timed(c::AbstractCompositeModel, x::SVector, vars, others; tol::Float64 = 1.0e-9, itermax = 1.0e4, verbose::Bool = false)
+
+    it = 0
+    er = Inf
+    local α
+    to = TimerOutput()
+    @timeit to "Newton-Raphson Iterations" while er > tol
+        it += 1
+        @timeit to "residual" r = compute_residual(c, x, vars, others)
+        @timeit to "jacobian" J = ForwardDiff.jacobian(y -> compute_residual(c, y, vars, others), x)
+        @timeit to "backlash" Δx = J \ r
+        @timeit to "line search" α = bt_line_search(Δx, J, x, r, c, vars, others)
+        @timeit to "update solution" x -= α .* Δx
+        # check convergence
+        @timeit to "convergence check" er = mynorm(Δx, x)
+
+        it > itermax && break
+    end
+    display(to)
+    if verbose
+        println("Iterations: $it, Error: $er, α = $α")
+    end
+    return x
+end
+
 
 function bt_line_search(Δx, J, x, r, composite, vars, others; α = 1.0, ρ = 0.5, c = 1.0e-4, α_min = 1.0e-8)
 
