a56fa93b307856561006170e5447ff048fbf43b3
tduretz <duretz@uni-frankfurt.de>
2025-08-01 16:02:49 +0200

    add convergence test

--- examples/Maxwell_VE.jl
+++ examples/Maxwell_VE.jl
@@ -1,21 +1,20 @@
 using RheologyCalculator
 import RheologyCalculator: compute_stress_elastic, compute_pressure_elastic
 
-using GLMakie
+using GLMakie, MathTeXEngine
+import Statistics: mean
+Makie.update_theme!( fonts = (regular = texfont(), bold = texfont(:bold), italic = texfont(:italic)))
 
 analytical_solution(ϵ, t, G, η) = 2 * ϵ * η * (1 - exp(-G * t / η))
 
 function stress_time(c, vars, x; ntime = 200, dt = 1.0e8)
-    # Extract elastic stresses/pressure from solutio vector
+    # Extract elastic stresses/pressure from solution vector
     τ1   = zeros(ntime)
     τ_an = zeros(ntime)
-    # τ2 = zeros(ntime)
-    # P1 = zeros(ntime)
-    # P2 = zeros(ntime)
-    t_v = zeros(ntime)
-    τ_e = (0.0,)
-    P_e = (0.0,)
-    t = 0.0
+    t_v  = zeros(ntime)
+    τ_e  = (0.0,)
+    P_e  = (0.0,)
+    t   = 0.0
     for i in 2:ntime
         others = (; dt = dt, τ0 = τ_e, P0 = P_e)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -41,32 +40,39 @@ c, x, vars, args, others = let
 
     c  = SeriesModel(viscous, elastic)
 
-    vars = (; ε = 1.0e-14, θ = 1.0e-20)         # input variables (constant)
-    args = (; τ = 2.0e3, P = 1.0e6)             # guess variables (we solve for these, differentiable)
-    others = (; dt = 1.0e10, τ0 = (0e0, ), P0 = (0.0, ))       # other non-differentiable variables needed to evaluate the state functions
+    vars   = (; ε = 1.0e-14, θ = 1.0e-20)                # input variables (constant)
+    args   = (; τ = 2.0e3, P = 1.0e6)                    # guess variables (we solve for these, differentiable)
+    others = (; dt = 1.0e10, τ0 = (0e0, ), P0 = (0.0, )) # other non-differentiable variables needed to evaluate the state functions
 
     x = initial_guess_x(c, vars, args, others)
 
     c, x, vars, args, others
 end
 
-t_v, τ, τ_an = stress_time(c, vars, x; ntime = 10_000, dt = 1e9)
+let
+    dt = 1e10 .* [1.0, 1/2, 1/4, 1/8]
+    ϵ  = zero(dt) 
+    for it in eachindex(dt)
+        t_v, τ, τ_an = stress_time(c, vars, x; ntime = 100_000, dt = dt[it])
+        ϵ[it] = maximum(abs.(τ .- τ_an))
+    end
 
-SecYear = 3600 * 24 * 365.25
-fig = Figure(fontsize = 30, size = (800, 600) .* 2)
-ax  = Axis(fig[1, 1], title = "Burgers model", xlabel = "t [kyr]", ylabel = L"\tau [MPa]")
-ax2 = Axis(fig[2, 1], title = "Burgers model", xlabel = "t [kyr]", ylabel = L"\tau [MPa]")
+    # Order
+    θ      = log(ϵ[1]/ϵ[2]) / log(dt[1]/dt[2])
+    dt_arr = LinRange(dt[end], dt[1], 100)
+    ϵ_arr  = 10 .^(log10.(ϵ[1]) .- θ.*( log10.(1 ./ (dt_arr)) .- log10.(1 ./ (dt_arr[end]))))
 
-lines!(ax, t_v / SecYear / 1.0e3, τ_an / 1.0e6, color=:black, label = "analytical")
-scatter!(ax, t_v / SecYear / 1.0e3, τ / 1.0e6,  color=:red, label = "numerical")
+    SecYear = 3600 * 24 * 365.25
+    fig = Figure(fontsize = 30, size = (800, 600) .* 1)
 
-lines!(ax2, t_v / SecYear / 1.0e3, log10.(abs.(τ_an.-τ) ./ τ_an), color=:black)
+    ax1 = Axis(fig[1, 1], title = L"$$Maxwell model", xlabel = L"$t$ [kyr]", ylabel = L"$\tau$ [MPa]")
+    lines!(ax1, t_v / SecYear / 1.0e3, τ_an / 1.0e6, color=:black, label = "analytical")
+    scatter!(ax1, t_v[1:1000:end] / SecYear / 1.0e3, τ[1:1000:end] / 1.0e6,  color=:red, label = "numerical")
 
-axislegend(ax, position = :rb)
-#title!(ax,"Burgers model")
-ax.xlabel = L"t [kyr]"
-ax.ylabel = L"\tau [MPa]"
+    ax2 = Axis(fig[2, 1], title = L"$$Convergence", xlabel = L"$t$ [kyr]", ylabel = L"$\tau$ [MPa]")
+    lines!(ax2, log10.(1 ./ dt_arr), log10.(ϵ_arr), color=:black, label="1st order")
+    scatter!(ax2, log10.(1 ./ dt), log10.(ϵ), color=:black, label="numerics")
 
-ax2.xlabel = L"t [kyr]"
-ax2.ylabel = L"\log_{10}\text{relative error}"
-display(fig)
+    axislegend()
+    display(fig)
+end
