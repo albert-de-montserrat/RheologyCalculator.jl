7fbba4c48e6632510df5b76824562948bac2f092
Boris Kaus <kaus@uni-mainz.de>
2025-08-21 15:53:58 +0200

    update time-dependent tests to account for normalisation

--- test/test_VEP.jl
+++ test/test_VEP.jl
@@ -7,7 +7,7 @@ using LinearAlgebra
         return τy < τ ? τy : τ
     end
 
-    function stress_time(c, vars, x, others; ntime = 200, dt = 1.0e8)
+    function stress_time(c, vars, x, xnorm, others; ntime = 200, dt = 1.0e8)
         # Extract elastic stresses/pressure from solutio vector
         τ1   = zeros(ntime)
         τ_an = zeros(ntime)
@@ -18,7 +18,7 @@ using LinearAlgebra
         for i in 2:ntime
             others = (; dt = dt, τ0 = τ_e, P = others.P, P0 = P_e)       # other non-differentiable variables needed to evaluate the state functions
 
-            x       = solve(c, x, vars, others, verbose = false)
+            x       = solve(c, x, vars, others, verbose = false, xnorm=xnorm)
             τ1[i]   = x[1]
             t      += others.dt
             τ_an[i] = analytical_solution(vars.ε, t, c.leafs[2].G, c.leafs[1].η, c.leafs[3].C, c.leafs[3].ϕ, others.P)
@@ -29,7 +29,7 @@ using LinearAlgebra
         return t_v, τ1, τ_an
     end
 
-    c, x, vars, args, others = let
+    c, x, xnorm, vars, args, others = let
 
         viscous = LinearViscosity(1e22)
         elastic = IncompressibleElasticity(10e9)
@@ -47,12 +47,15 @@ using LinearAlgebra
         # other non-differentiable variables needed to evaluate the state functions
         others = (; dt = 1.0e8, P = 1.0e6, τ0 = 0e0, P0 = 0.0)
 
-        x = initial_guess_x(c, vars, args, others)
+        x       = initial_guess_x(c, vars, args, others)
+        char_τ  = 1e6
+        char_ε  = vars.ε 
+        xnorm   = normalisation_x(c, char_τ, char_ε)
 
-        c, x, vars, args, others
+        c, x, xnorm, vars, args, others
     end
 
-    _, τ, τ_an = stress_time(c, vars, x, others; ntime = 1_500, dt = 1e8)
+    _, τ, τ_an = stress_time(c, vars, x, xnorm, others; ntime = 1_500, dt = 1e8)
     @test error = norm((abs.(τ_an .- τ) ./ τ_an)[2:end]) < 0.0015
     @test any(!isnan, τ)
 end
