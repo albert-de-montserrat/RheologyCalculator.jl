f33bd63acff19f329f010286ac98c61221a983d8
tduretz <duretz@uni-frankfurt.de>
2025-08-27 15:28:28 +0200

    add modified Cam ModCamClay

--- examples/Maxwell_ModCamClay.jl
+++ examples/Maxwell_ModCamClay.jl
@@ -2,7 +2,8 @@
 using Test, LinearAlgebra
 using RheologyCalculator
 import RheologyCalculator: compute_stress_elastic, compute_pressure_elastic
-using GLMakie
+using GLMakie, MathTeXEngine
+Makie.update_theme!( fonts = (regular = texfont(), bold = texfont(:bold), italic = texfont(:italic)))
 using StaticArrays
 
 include("RheologyDefinitions.jl")
@@ -46,7 +47,7 @@ c, x, xnorm, vars, args, others = let
 
     viscous = LinearViscosity(1e23)
     elastic = Elasticity(1e10, 2e11)
-    plastic = Hyperbolic(; C=1e6, ϕ=30.0, ψ=10.0, η_vp=0.0, Pt=-5e5) 
+    plastic = ModCamClay(; M=0.9, r=1e8, β=.1, Pt=-1e5, η_vp=1e20) 
 
     # Maxwell viscoelastic model
     c  = SeriesModel(viscous, elastic, plastic)
@@ -59,7 +60,7 @@ c, x, xnorm, vars, args, others = let
     others = (; dt = 1.0e5, τ0 = (0e0, ), P0 = (0.3e6, ))
 
     x       = initial_guess_x(c, vars, args, others)
-    char_τ  = plastic.C
+    char_τ  = plastic.r
     char_ε  = vars.ε+vars.θ
     xnorm   = normalisation_x(c, char_τ, char_ε)
 
@@ -67,8 +68,8 @@ c, x, xnorm, vars, args, others = let
 end
 
 # Plot yield stress - this is reproducing Fig. 2 of the paper
-τII = 0:0.01e6:2e6
-P   = -1e6:0.01e6:2e6
+τII = 0:1e6:1e8
+P   = -5e6:1e6:1.5e8
 
 F = zeros(length(P), length(τII))
 Q = zeros(length(P), length(τII))
@@ -79,33 +80,37 @@ end
 
 # Reproduce Fig. 5 of Popov et al. (2025)
 fig = Figure(fontsize = 20, size = (800, 800) )
-ax1 = Axis(fig[1,1], title="Volumetric extension",  xlabel=L"$t$ [yr]",  ylabel=L"$\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
-ax2 = Axis(fig[1,2], title="Deviatoric shear",      xlabel=L"$t$ [yr]",  ylabel=L"$\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
-ax3 = Axis(fig[2,1], title="Vol + Dev shear",       xlabel=L"$t$ [yr]",  ylabel=L"$\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
-ax4 = Axis(fig[2,2], title="",                      xlabel=L"$P$ [MPa]", ylabel=L"$\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
+ax1 = Axis(fig[1,1], title="Volumetric extension (1)",  xlabel=L"$t$ [yr]",  ylabel=L"$P$, $\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
+ax2 = Axis(fig[1,2], title="Volumetric compaction (2)",      xlabel=L"$t$ [yr]",  ylabel=L"$P$, $\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
+ax3 = Axis(fig[2,1], title="Vol. + Dev. shear (3)",       xlabel=L"$t$ [yr]",  ylabel=L"$P$, $\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
+ax4 = Axis(fig[2,2], title="Stress path",                      xlabel=L"$P$ [MPa]", ylabel=L"$\tau_{II}$ [MPa]", xlabelsize=20, ylabelsize=20)
 
 SecYear = 3600 * 24 * 365.25
 t_v1, τ1, P1, F1, mode2_1 = stress_time(c, (; ε = 0*7.0e-14, θ =   7.0e-15), x, xnorm, others; ntime = 11, dt = SecYear*2)
 println("-------")
-t_v2, τ2, P2, F2, mode2_2 = stress_time(c, (; ε =   7.0e-14, θ = 0*7.0e-15), x, xnorm, others; ntime = 80, dt = 1e7)
+t_v2, τ2, P2, F2, mode2_2 = stress_time(c, (; ε =   0*7.0e-14, θ = -7.0e-15), x, xnorm, others; ntime = 1300, dt = 1e8)
 println("-------")
-t_v3, τ3, P3, F3, mode2_3 = stress_time(c, (; ε =   7.0e-14, θ =   7.0e-15), x, xnorm, others; ntime = 30, dt = 2e7)
+t_v3, τ3, P3, F3, mode2_3 = stress_time(c, (; ε =   7.0e-14, θ =   -4.0e-15), x, xnorm, others; ntime = 700, dt = 1e8)
 println("-------")
 
-lines!(ax1, t_v1 / SecYear , P1 / 1.0e6, color=:red, label = L"P")
-lines!(ax1, t_v1 / SecYear , τ1 / 1.0e6,  color=:blue, label =  L"\tau_{II}")
+lines!(ax1, t_v1 / SecYear , P1 / 1.0e6, color=:red, label = L"$P$")
+lines!(ax1, t_v1 / SecYear , τ1 / 1.0e6,  color=:blue, label =  L"$\tau_{II}$")
 
-lines!(ax2, t_v2 / SecYear , P2 / 1.0e6, color=:red, label = L"P")
-lines!(ax2, t_v2 / SecYear , τ2 / 1.0e6,  color=:blue, label =  L"\tau_{II}")
+lines!(ax2, t_v2 / SecYear , P2 / 1.0e6, color=:red, label = L"$P$")
+lines!(ax2, t_v2 / SecYear , τ2 / 1.0e6,  color=:blue, label =  L"$\tau_{II}$")
 
-lines!(ax3, t_v3 / SecYear , P3 / 1.0e6, color=:red, label = L"P")
-lines!(ax3, t_v3 / SecYear , τ3 / 1.0e6,  color=:blue, label =  L"\tau_{II}")
+lines!(ax3, t_v3 / SecYear , P3 / 1.0e6, color=:red, label = L"$P$")
+lines!(ax3, t_v3 / SecYear , τ3 / 1.0e6,  color=:blue, label =  L"$\tau_{II}$")
+axislegend(ax3, position=:rb)
 
-# GLMakie.contour!(ax4, P/1e6, τII/1e6, F, levels = [0.01], color = :black)
-# GLMakie.scatter!(ax4, P1/1e6, τ1/1e6, color = :green)
-# GLMakie.scatter!(ax4, P2/1e6, τ2/1e6, color = :red)
-# GLMakie.scatter!(ax4, P3/1e6, τ3/1e6, color = :blue)
-contourf!(ax4, P/1e6, τII/1e6, Q)
+
+GLMakie.contour!(ax4, P/1e6, τII/1e6, F, levels = [0.01], color = :black)
+GLMakie.scatter!(ax4, P2/1e6, τ2/1e6, color = :red, label=L"1")
+GLMakie.scatter!(ax4, P3/1e6, τ3/1e6, color = :blue, label=L"2")
+GLMakie.scatter!(ax4, P1/1e6, τ1/1e6, color = :green, label=L"3")
+axislegend(ax4, position=:lt)
+
+# contourf!(ax4, P/1e6, τII/1e6, Q)
 
 display(fig)
 
--- examples/ModCamClay.jl
+++ examples/ModCamClay.jl
@@ -4,92 +4,93 @@
 using RheologyCalculator
 import RheologyCalculator: compute_stress_elastic, compute_pressure_elastic
 
-# Hyperbolic ------------------------------------------------------
+# ModCamClay ------------------------------------------------------
 """
-    Hyperbolic{T} <: AbstractPlasticity
+    ModCamClay{T} <: AbstractPlasticity
 
-Represents a Drucker-Prager plasticity model with cap for pressure-dependent yielding of mode-1 and mode-2 plasticity, 
-as described in Popov et al. (2025), Geoscientific Model Development
+Represents a Modified Cam-Clay model, see de Souza Neto book (p. 404)
 
 # Fields
-- `C::T`: The cohesion parameter.
-- `ϕ::T`: The friction angle (in degrees).
-- `ψ::T`: The dilatancy angle (in degrees).
-- `η_vp::T`: The Duvaut-Lions regeularisation viscosity for the plasticity model.
-- `Pt::T`: The tensile strength (should be < 0).
+- `M::T`    : The flattening factor (1: circle, < 1: ellipse) = 0.9
+- `r::T`    : The radius along pressure axis = 2e8
+- `β::T`    : The asymmetry for compaction cap (1:symmetric, <1 asymmetric)  = 0.1
+- `Pt::T`   : The Tensile strength
+- `η_vp::T` : The regularisation viscosity
 """
-struct Hyperbolic{T} <: AbstractPlasticity
-    C::T
-    ϕ::T        # in degrees for now
-    ψ::T        # in degrees for now
-    η_vp::T     # regularisation viscosity
+struct ModCamClay{T} <: AbstractPlasticity
+    M::T        # flattening factor (1: circle, < 1: ellipse) = 0.9
+    r::T        # radius along pressure axis = 2e8
+    β::T        # asymmetry for compaction cap (1:symmetric, <1 asymmetric)  = 0.1
     Pt::T       # Tensile strength
-
-    # computational parameters (precomputed, to speed up later calculations)
-    sinϕ::T     # Friction angle
-    cosϕ::T     # Friction angle
-    sinΨ::T     # Dilation angle 
-    cosΨ::T     # Dilation angle
+    η_vp::T     # regularisation viscosity
 end
 
-function Hyperbolic(; C=10e6, ϕ=30.0, ψ=0.0, η_vp=1e20, Pt=-1e5) 
-    sinϕ = sind(ϕ) # Friction angle
-    cosϕ = cosd(ϕ) # Friction angle
-    sinΨ = sind(ψ) # Dilation angle
-    cosΨ = cosd(ψ) # Dilation angle
-    return Hyperbolic(C, ϕ, ψ, η_vp, Pt, sinϕ, cosϕ, sinΨ, cosΨ)
+function ModCamClay(; M=0.9, r=1e8, β=1.0, Pt=-1e7, η_vp=1e20) 
+    return ModCamClay(M, r, β, Pt, η_vp)
 end
 
-#Hyperbolic(args...) = Hyperbolic(promote(args...)...)
-@inline _isvolumetric(::Hyperbolic) = true
+#ModCamClay(args...) = ModCamClay(promote(args...)...)
+@inline _isvolumetric(::ModCamClay) = true
 
-@inline series_state_functions(::Hyperbolic) = (compute_strain_rate, compute_lambda, compute_volumetric_strain_rate)
-@inline parallel_state_functions(::Hyperbolic) = compute_stress, compute_pressure, compute_lambda, compute_plastic_strain_rate, compute_volumetric_plastic_strain_rate
+@inline series_state_functions(::ModCamClay) = (compute_strain_rate, compute_lambda, compute_volumetric_strain_rate)
+@inline parallel_state_functions(::ModCamClay) = compute_stress, compute_pressure, compute_lambda, compute_plastic_strain_rate, compute_volumetric_plastic_strain_rate
 
-@inline function compute_strain_rate(r::Hyperbolic; τ = 0, λ = 0, P = 0, kwargs...)
-    ε_pl = compute_plastic_strain_rate(r::Hyperbolic; τ_pl = τ, λ = λ, P_pl = P, kwargs...)
+@inline function compute_strain_rate(r::ModCamClay; τ = 0, λ = 0, P = 0, kwargs...)
+    ε_pl = compute_plastic_strain_rate(r::ModCamClay; τ_pl = τ, λ = λ, P_pl = P, kwargs...)
     F = compute_F(r, τ, P)
     return ε_pl/2* (F > -1e-8)
 end
-@inline function compute_volumetric_strain_rate(r::Hyperbolic; τ = 0, λ = 0, P = 0, kwargs...)
-    θ_pl = compute_volumetric_plastic_strain_rate(r::Hyperbolic; τ_pl = τ, λ = λ, P_pl = P, θ = 0, kwargs...)
+@inline function compute_volumetric_strain_rate(r::ModCamClay; τ = 0, λ = 0, P = 0, kwargs...)
+    θ_pl = compute_volumetric_plastic_strain_rate(r::ModCamClay; τ_pl = τ, λ = λ, P_pl = P, θ = 0, kwargs...)
     F    = compute_F(r, τ, P)
     return θ_pl* (F > -1e-8) # perhaps this derivative needs to be hardcoded
 end
 
-@inline function compute_lambda(r::Hyperbolic; τ = 0, λ = 0, P = 0, kwargs...)
+@inline function compute_lambda(r::ModCamClay; τ = 0, λ = 0, P = 0, kwargs...)
     F = compute_F(r, τ, P)
     return -F* (F > -1e-8)  + λ*r.η_vp + λ*1        # last term is for regularisation below yield
 end
 
-function compute_F(r::Hyperbolic, τII, P)
-    cosϕ, sinϕ, C, Pt = r.cosϕ, r.sinϕ, r.C, r.Pt
+function compute_F(r::ModCamClay, τII, P)
+    M, r, β, Pt = r.M, r.r, r.β, r.Pt
 
-    F  = sqrt(τII^2 + (C * cosϕ + Pt*sinϕ)^2) - (P * sinϕ + C * cosϕ)
+    if P < Pt + r 
+        b = 1
+    else
+        b = β
+    end 
+
+    F  = 1/b *(P - Pt - r)^2  + (τII)^2 / M^2 - r^2 
 
     # Note that viscoplastic regularisation is taken into account in the residual function
     return F #*(F>-1e-8) 
 end
 
-function compute_Q(r::Hyperbolic, τII, P) 
+function compute_Q(r::ModCamClay, τII, P) 
 
     # These parameters are required to compute the constant in the plastic flow
     # potential. Note that this constant does not matter apart when plotting,
     # as we only need derivates of Q in general 
-    cosΨ, sinΨ, C, Pt = r.cosΨ, r.sinΨ, r.C, r.Pt
+    M, r, β, Pt = r.M, r.r, r.β, r.Pt
 
-    Q  =  sqrt(τII^2 + (C * cosΨ + Pt*sinΨ)^2) - (P * sinΨ + C * cosΨ)
+    if P < Pt + r 
+        b = 1
+    else
+        b = β
+    end 
+
+    Q  = 1/b *(P - Pt - r)^2  + (τII)^2 / M^2 - r^2 
 
     return Q
 end 
 
-@inline compute_stress(r::Hyperbolic; τ_pl = 0, kwargs...) = τ_pl
-@inline compute_pressure(r::Hyperbolic; P_pl = 0, kwargs...) = P_pl
+@inline compute_stress(r::ModCamClay; τ_pl = 0, kwargs...) = τ_pl
+@inline compute_pressure(r::ModCamClay; P_pl = 0, kwargs...) = P_pl
 
-@inline function compute_plastic_strain_rate(r::Hyperbolic; τ_pl = 0, λ = 0, P_pl = 0, ε = 0, kwargs...)
+@inline function compute_plastic_strain_rate(r::ModCamClay; τ_pl = 0, λ = 0, P_pl = 0, ε = 0, kwargs...)
     return λ*ForwardDiff.derivative(x -> compute_Q(r, x, P_pl), τ_pl) - 0*ε # perhaps this derivative needs to be hardcoded 
 end
 
-@inline function compute_volumetric_plastic_strain_rate(r::Hyperbolic; τ_pl = 0, λ = 0, P_pl = 0, θ = 0, kwargs...)
+@inline function compute_volumetric_plastic_strain_rate(r::ModCamClay; τ_pl = 0, λ = 0, P_pl = 0, θ = 0, kwargs...)
     return -λ * ForwardDiff.derivative(x -> compute_Q(r, τ_pl, x), P_pl) - 0*θ # perhaps this derivative needs to be hardcoded
 end
