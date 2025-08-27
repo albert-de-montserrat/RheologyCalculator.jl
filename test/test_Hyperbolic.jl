8c2600e081cd6e957e324928dd34a3ba9002326a
tduretz <duretz@uni-frankfurt.de>
2025-08-27 16:16:03 +0200

    try to add a test for hyperbolic

---
+++ test/test_Hyperbolic.jl
@@ -0,0 +1,56 @@
+using LinearAlgebra
+
+# using RheologyCalculator
+# include("../examples/RheologyDefinitions.jl") 
+# include("../examples/Hyperbolic.jl")
+
+# @testset "VEVP Model " begin
+    function stress_time(c, vars, x, xnorm, others; ntime = 200, dt = 1.0e8)
+        # Extract elastic stresses/pressure from solutio vector
+        τ1  = zeros(ntime)
+        t_v = zeros(ntime)
+        τ_e = (0.0,)
+        P_e = (0.0,)
+        t   = 0.0
+        for i in 2:ntime
+            others   = (; dt = dt, τ0 = τ_e, P=others.P, P0 = P_e)       # other non-differentiable variables needed to evaluate the state functions
+            x        = solve(c, x, vars, others, verbose = false, xnorm=xnorm)
+            τ1[i]    = x[1]
+            t       += others.dt
+            τ_e      = x[1] 
+            t_v[i]   = t
+        end
+        return t_v, τ1
+    end
+
+    c, x, xnorm, vars, args, others = let
+        viscous     = LinearViscosity(1e22)
+        viscous_reg = LinearViscosity(1e20)
+        elastic     = IncompressibleElasticity(10e9)
+        plastic     = Hyperbolic(; C=1e6, ϕ=30.0, ψ=5.0, η_vp=0.0, Pt=-5e5) 
+
+        # Maxwell visco-elasto-(visco-plastic) model
+        p  = ParallelModel(viscous_reg, plastic)
+        c  = SeriesModel(viscous, elastic, p)
+
+        # input variables (constant)
+        vars   = (; ε = 1.0e-14)
+        # guess variables (we solve for these, differentiable)
+        args   = (; τ = 2.0e3, λ = 0)
+        # other non-differentiable variables needed to evaluate the state functions
+        others = (; dt = 1.0e8, τ0 = (0e0, ), P = 1.0e6, P0 = (0.0, ))
+
+        x = initial_guess_x(c, vars, args, others)
+        
+        char_τ = 1e6
+        char_ε = vars.ε 
+        xnorm  = normalisation_x(c, char_τ, char_ε)
+
+        c, x, xnorm, vars, args, others
+    end
+
+    _, τ = stress_time(c, vars, x, xnorm, others; ntime = 1_500, dt = 1e8)
+    @show maximum(τ)
+#     @test maximum(τ) == 1.1049696158565428e7
+#     @test any(!isnan, τ)
+# end
