4f09307340996c80d06e1e760c78d6d41bb6bc85
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-09-02 16:39:49 +0200

    WIP

--- src/strain_rate_correction.jl
+++ src/strain_rate_correction.jl
@@ -1,22 +1,5 @@
-
-# @inline effective_strain_rate_correction(c::SeriesModel, vars, others) = effective_strain_rate_correction(c.leafs, c.branches, vars, others)
-
-# @generated function effective_strain_rate_correction(leafs::NTuple{N, Any}, ::Tuple{}, vars, others) where {N}
-#     quote
-#         i = 0
-#         ε_elastic_cor = zero(vars.ε)
-#         Base.@nexprs $N j -> begin
-#             if isa(leafs[j], AbstractElasticity)
-#                 i += 1
-#                 # compute local effect on strainrate tensor due to old elastic stresses
-#                 η = compute_viscosity_series(leafs[j], merge(vars, others))
-#                 ε_elastic_cor += others.τ0[i] / (2 * η)
-#             end
-#         end
-    
-#         return ε_elastic_cor
-#     end
-# end
+@inline second_invariant(xx, yy, xy) = √(0.5 * (xx^2 + yy^2) + xy^2)
+@inline second_invariant(xx, yy, zz, yz, xz, xy) = √(0.5 * (xx^2 + yy^2 + zz^2) + xy^2 + yz^2 + xz^2)
 
 @generated function effective_strain_rate_correction(c::SeriesModel, ε::NTuple{N}, τ0::NTuple{N}, others) where N
     quote
@@ -43,5 +26,19 @@ end
     end
 end
 
-@inline second_invariant(xx, yy, xy) = √(0.5 * (xx^2 + yy^2) + xy^2)
-@inline second_invariant(xx, yy, zz, yz, xz, xy) = √(0.5 * (xx^2 + yy^2 + zz^2) + xy^2 + yz^2 + xz^2)
+@generated function compute_effective_viscosity(branches::NTuple{N, Any}, ::Tuple{}, ε, others) where {N}
+    quote
+        η_eff = zero(eltype(ε))
+        Base.@nexprs $N j -> begin
+            if isa(branches[j], AbstractElasticity)
+                # compute local effect on strainrate tensor due to old elastic stresses
+                η = compute_viscosity_parallel(branches[j], merge((; ε), others))
+                @show η
+                η_eff += 1 / η
+            end
+        end
+        @show N
+        η_eff = 1 / η_eff
+        return η_eff
+    end
+end
