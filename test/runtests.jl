b108563760db29370da609a084ef88709893ab30
albert-de-montserrat <albert.de.montserrat@gmail.com>
2026-07-15 10:47:34 +0200

    update CI workflow to skip cov in allocations test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,13 @@ function runtests()
     files = readdir(@__DIR__)
     test_files = filter(startswith("test_"), files)
 
+    allocations_only = "--allocations-only" in ARGS
+    if allocations_only
+        test_files = filter(==("test_allocations.jl"), test_files)
+    elseif Base.JLOptions().code_coverage != 0
+        filter!(!=("test_allocations.jl"), test_files)
+    end
+
     for f in test_files
         !isdir(f) && include(f)
     end
